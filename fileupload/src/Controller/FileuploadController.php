95ff6ba1dc6f5628f2aefead21411d986204ee9f
Nitin <nitin.kawane@gmail.com>
2022-12-24 21:35:58 +0530

    Update FileuploadController.php

--- fileupload/src/Controller/FileuploadController.php
+++ fileupload/src/Controller/FileuploadController.php
@@ -13,38 +13,39 @@ use Drupal\Core\Url;
 
 class FileuploadController extends ControllerBase {
 
-    /**
-     * Display.
-     * @return string
-     */
-    public function display() {
-        $header_table = array(
-            'id'=>    t('SrNo'),
-            'employee_name' => t('Employee Name'),
-            'document_name' => t('Document Name'),
-            'action' => t('Action'),
-           );
+  /**
+   * Display.
+   * @return string
+   */
+  public function display() {
+    $header_table = [
+      'id'=>    t('SrNo'),
+      'employee_name' => t('Employee Name'),
+      'document_name' => t('Document Name'),
+      'action' => t('Action'),
+    ];
 
-        $query = \Drupal::database()->select('fileupload', 'f');
-        $query->fields('f', ['id','employee_name','document_name']);
-        $results = $query->execute()->fetchAll();
-        $rows=array();
-        foreach($results as $data){
-            $view = Url::fromUserInput('/sites/default/files/my_files/'.$data->document_name);
-            $rows[] = array(
-                'id' =>$data->id,
-                'employee_name' => $data->employee_name,
-                'document_name' => $data->document_name,
-                \Drupal::l('View File', $view),
-            );
-        }
-        $form['table'] = [
-            '#type' => 'table',
-            '#header' => $header_table,
-            '#rows' => $rows,
-            '#empty' => t('No users found'),
-        ];
-        return $form;
+    $query = \Drupal::database()->select('fileupload', 'f');
+    $query->fields('f', ['id','employee_name','document_name']);
+    $results = $query->execute()->fetchAll();
+    $rows=array();
+    foreach($results as $data){
+      $view = Url::fromUserInput('/sites/default/files/my_files/'.$data->document_name);
+      $rows[] = [
+        'id' =>$data->id,
+        'employee_name' => $data->employee_name,
+        'document_name' => $data->document_name,
+        \Drupal::l('View File', $view),
+      ];
     }
+    
+    $form['table'] = [
+      '#type' => 'table',
+      '#header' => $header_table,
+      '#rows' => $rows,
+      '#empty' => t('No users found'),
+    ];
+    return $form;
+  }
   
 }
